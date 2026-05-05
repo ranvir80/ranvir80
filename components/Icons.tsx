64211b7182f0e31c5aa3f35ecac6a7cb3edaca93
Ranvir Pardeshi <130527349+ranvir80@users.noreply.github.com>
2026-05-05 20:37:58 +0530

    Update Icons.tsx

--- components/Icons.tsx
+++ components/Icons.tsx
@@ -73,3 +73,9 @@ export const SupportIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
         <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z" />
     </svg>
 );
+
+export const ChevronDownIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" {...props}>
+        <path strokeLinecap="round" strokeLinejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+    </svg>
+);
